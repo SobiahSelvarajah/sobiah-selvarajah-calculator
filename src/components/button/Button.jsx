eefe5909bc89bd5845919aa6bf12bb13334093e9
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-04-02 22:06:33 +0100

    button component added

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = ({char}) => {
+  return (
+    <button className='button__wrapper'>{char}</button>
+  )
+}
+
+export default Button;
--- src/components/calc/Calc.jsx
+++ src/components/calc/Calc.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-
+import Button from '../button/Button';
 
 const Calc = () => {
   return (
     <main className='calc__wrapper'>
         <div className='calc__row'>
+            <Button char="7"/>
+            <Button char="8"/>
+            <Button char="9"/>
+            <Button char="/"/>
+        </div>
+        <div className='calc__row'>
+            <Button char="4"/>
+            <Button char="5"/>
+            <Button char="6"/>
+            <Button char="x"/>
+        </div>
+        <div className='calc__row'>
+            <Button char="1"/>
+            <Button char="2"/>
+            <Button char="3"/>
+            <Button char="+"/>
+        </div>
+        <div className='calc__row'>
+            <Button char="0"/>
+            <Button char="."/>
+            <Button char="="/>
+            <Button char="-"/>
         </div>
     </main>
   )
 }
 
-export default Calc
+export default Calc;
